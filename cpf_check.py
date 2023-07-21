9bac5ae98bb69811786b273bad3457affa803f88
julioesf <julioesf@gmail.com>
2023-07-21 19:39:21 -0300

    Finish CPF validator

--- cpf_check.py
+++ cpf_check.py
@@ -1,22 +1,47 @@
-# cpf = input("Type the CPF (only numbers): ")
-cpf = '74682489070'
+import re
+cpf = input("Type the CPF: ")
+# cpf = '74682489070'
+# cpf = '47505406643'
+# cpf = '36598906874'
+# cpf = '03636157680'
+cpf = str(re.sub(
+    r'[^0-9]',
+    '',
+    cpf))
+
 multiplicator = 10
 check = 0
-
 for digit in cpf:
     if multiplicator > 1:
         check += int(digit) * multiplicator
         multiplicator -= 1
-check *= 10
-dec_digit = check % 11
-if dec_digit > 9:
-    if int(cpf[9]) == 0:
-        print("First digit is correct!")
+dec_digit = (check * 10) % 11
+if dec_digit > 9 and int(cpf[9]) == 0:
+    multiplicator = 11
+    check = 0
+    for digit in cpf:
+        if multiplicator > 1:
+            check += int(digit) * multiplicator
+            multiplicator -= 1
+    dec_digit = (check * 10) % 11
+    if dec_digit > 9 and int(cpf[10]) == 0:
+        print(f'CPF {cpf} is Valid!')
+    elif dec_digit <= 9 and int(cpf[10]) == dec_digit:
+        print(f'CPF {cpf} is valid!')
+elif dec_digit == int(cpf[9]):
+    multiplicator = 11
+    check = 0
+    for digit in cpf:
+        if multiplicator > 1:
+            check += int(digit) * multiplicator
+            multiplicator -= 1
+    dec_digit = (check * 10) % 11
+    if dec_digit > 9 and int(cpf[10]) == 0:
+        print(f'CPF {cpf} is Valid!')
+    elif dec_digit <= 9 and int(cpf[10]) == dec_digit:
+        print(f'CPF {cpf} is valid!')
     else:
-        print("***First digit is wrong***")
+        print(f'***CPF {cpf} is invalid***')
 else:
-    if int(cpf[9]) == dec_digit:
-        print('First digit is correct!"')
-    else:
-        print("***First digit is wrong***")
+    print(f'***CPF {cpf} is invalid***')
 
